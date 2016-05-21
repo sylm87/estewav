a7189d171be66a6e986299d5645edfcac87a2e0e
sylm87 <christian472006@gmail.com>
2016-05-21 22:08:06 +0200

    proyecto en alpha v0.2

--- codificador.py
+++ codificador.py
@@ -2,76 +2,123 @@
 # -*- coding: UTF-8 -*-
 
 # "Esteganografía con ficheros de audio WAV"
+# Archivo: codificador.py
 # Autor: Christian Prieto Bustamante
 
 import wave
 
 
-jumpsData = 20 #Espacio de muestras del framerate para la toma de datos válidos
+jumpsData = 10 #Espacio de muestras del framerate para la toma de datos válidos
+numFraData = 1 #El dato (1 byte) se compone de la lectura de 1 framerate
+numFraCipher = 1 #Un framerate para configurar el cifrado
+numFraElemHead = 1 #cada elemento de la cabecera (para saber si se trata de un archivo generado por nosotros) ocupa un framerate
+numFraSize = 4 # 32bits para determinar cuantos bytes ocupa el fichero oculto
 
-numFraData = 2 #El dato (1 byte) se compone de la lectura de 2 tomas del framerate
-numFraCipher = 1 
-numFraElemHead = 1
+cipherPskEnable = 1
+cipherDisable = 0
 
-bitsCipherEnable = b"00000001"
+header = ["S", "Y", "L", "M"] # 1 byte por cada caracter (contra menos caracteres, más posibilidades de error por accidente)
 
-numBitsData = [5, 4] #se toman 5 bits de datos en la primera muestra y 4 bits en la segunda 
-#(el bit sobrante del octeto byte es para control (es el más significativo), y el resto de 
-#bits son los menos significantes del dato, para que no afecte negativamente al audio)
+#print ord(header[0])
+#print ord(header[1])
+#print ord(header[2])
+#print ord(header[3])
+#exit(0)
 
-bitsDatoControl = [b"10000", b"0000"] #en decimal 256 (b"100000000")
+#
+# COMPOSICIÓN DE LA TRAMA DE DATOS:
+#
+#  (CABECERA)->(CIFRADO)->(TAMAÑO ARCHIVO)->(BYTES ARCHIVO)
+#
 
-cabeceraInicio = ["S", "Y", "L", "M"] # 1 byte por cada caracter
+# DATOS DEL USUARIO:
+pathFileToHide = "calico.jpg"
+pathFileWav = "audioBase3.wav"
+pathFileWavResult = "estego.wav"
 
-repControlFinal = 4 # número de veces que se tiene que repetir los bits de control para finalizar la lectura
+# inicializamos a 0 el tamaño de bytes de los ficheros para tratamiento de errores:
+# El del fichero a ocultar es el tamaño total de bytes que ocupa en disco. El de el fichero .wav
+# donde lo vamos a ocultar el tamaño se refiere a los bytes totales del samplerate del octeto 
+# menos significativo de cada canal
+sizeFileToHide = 0 
+sizeFileWav = 0
 
 
 #TRATANDO CON LOS FICHEROS:
 
-# FICHERO QUE QUEREMOS OCULTAR
-ocultar = "calico.jpg"
-fileOcultar = open(ocultar,'rb')
-bytesFileOcultar = bytearray(fileOcultar.read())
-print int(bytesFileOcultar[1])
-
-
 # FICHERO BASE WAV DONDE LO VAMOS A OCULTAR
-audioBase = "audioBase1.wav"
-fileAudioBase = wave.open(audioBase,'rb')
-bytesFileAudioBase = bytearray(fileAudioBase.readframes(fileAudioBase.getnframes()))
-
-print "bytes fichero a ocultar:"
-print len(bytesFileOcultar)
-print len(bytesFileAudioBase) / 4 #2 canales de 16 bits cada unos (por eso se divide entre 4 para compararlo con en num de frames)
-print fileAudioBase.getnframes()
-
-print fileAudioBase.getsampwidth() # bytes del tamaño del sample
-
-inicioLectura = 0
-saltos=1
-if fileAudioBase.getsampwidth() == 2:
-	inicioLectura = 0
-	saltos = 2
+fileWav = wave.open(pathFileWav,'rb')
+bytesfileWav = bytearray(fileWav.readframes(fileWav.getnframes()))
 
 
-for i in range(4):
-	print bytesFileAudioBase[i]
+channels = fileWav.getnchannels() # canales de audio
+sampleWidth = fileWav.getsampwidth() # número de bytes para las muestras del samplerate
+frameRate = fileWav.getframerate()
+numTotalFrames = fileWav.getnframes()
+
+bytesMaximos = ((numTotalFrames / sampleWidth) / jumpsData) - len(header) - 1 # el -1 es para evitar Bytes huérfanos
+
+print "Número de canales: ",channels
+print "Ancho de muestra del sample (bytes): ",sampleWidth
+print "Número de frames totales: ", numTotalFrames
+print "Capacidad máxima de almacenado: ", bytesMaximos, "Bytes (",float(bytesMaximos / 1024), "KBytes )"
+
+
+
+# FICHERO QUE QUEREMOS OCULTAR
+try:
+	fileToHide = open(pathFileToHide,'rb')
+except:
+	print "ERROR: problema con fichero de audio y/o incompatible"
+	exit(1)
 	
-indexFileOculto = 0
-indexAudioBase = inicioLectura
-while indexFileOculto < len(bytesFileOcultar):
-	bytesFileAudioBase[indexAudioBase] = bytesFileOcultar[indexFileOculto]
-	indexAudioBase += saltos
-	indexFileOculto += 1
+bytesFileToHide = bytearray(fileToHide.read())
+
+if len(bytesFileToHide) < bytesMaximos:
+	print "El archivo a ocultar ocupa: ", len(bytesFileToHide), "Bytes (OK!)"
+else:
+	print "ERROR: El fichero que desea ocultar es demasiado grande"
+	exit(0)
+
+
+startIn = 0
+indexHeader = 0
+indexFileToHide = 0
+indexSampleWav = startIn
+
+while indexHeader < len(header):
+	bytesfileWav[indexSampleWav] = header[indexHeader]
+	indexHeader += 1
+	indexSampleWav += (sampleWidth * jumpsData)
+	
+bytesfileWav[indexSampleWav] = cipherDisable
+indexSampleWav += (sampleWidth * jumpsData)
+
+desplazamiento = 0
+for i in range(numFraSize):
+	
+	#print bytes(len(bytesFileToHide)>>24 & 0xFF) , bytes(len(bytesFileToHide)>>16 & 0xFF) , bytes(len(bytesFileToHide)>>8 & 0xFF) , bytes(len(bytesFileToHide)>>0 & 0xFF)
+	bytesfileWav[indexSampleWav] = len(bytesFileToHide)>>desplazamiento & 0xFF
+	desplazamiento += 8
+	indexSampleWav += (sampleWidth * jumpsData)
+
+
+
+while indexFileToHide < len(bytesFileToHide):
+	bytesfileWav[indexSampleWav] = bytesFileToHide[indexFileToHide]
+	indexSampleWav += (sampleWidth * jumpsData)
+	indexFileToHide += 1
+
+
 
 
 # GRABAMOS EL FICHERO DE AUDIO RESULTANTE:
-salidaEstegoWAV = wave.open("estego.wav", "wb") 
-salidaEstegoWAV.setnchannels(2) 
-salidaEstegoWAV.setsampwidth(16 / 8) 
-salidaEstegoWAV.setframerate(11025) 
-salidaEstegoWAV.writeframes(bytesFileAudioBase) 
-salidaEstegoWAV.close() 
+outEstegoWAV = wave.open(pathFileWavResult, "wb") 
+outEstegoWAV.setnchannels(channels) 
+outEstegoWAV.setsampwidth(sampleWidth) 
+outEstegoWAV.setframerate(frameRate) 
+outEstegoWAV.writeframes(bytesfileWav) 
+outEstegoWAV.close() 
 
 
 
--- decodificador.py
+++ decodificador.py
@@ -2,35 +2,102 @@
 # -*- coding: UTF-8 -*-
 
 # "Esteganografía con ficheros de audio WAV"
+# Archivo: decodificador.py
 # Autor: Christian Prieto Bustamante
 
 import wave
 
-# FICHERO BASE WAV DONDE LO VAMOS A OCULTAR
-audioBase = "estego.wav"
-fileAudioBase = wave.open(audioBase,'rb')
+
+jumpsData = 10 #Espacio de muestras del framerate para la toma de datos válidos
+numFraData = 1 #El dato (1 byte) se compone de la lectura de 1 framerate
+numFraCipher = 1 #Un framerate para configurar el cifrado
+numFraElemHead = 1 #cada elemento de la cabecera (para saber si se trata de un archivo generado por nosotros) ocupa un framerate
+numFraSize = 4 # 32bits para determinar cuantos bytes ocupa el fichero oculto
+
+# Constantes de cifrado
+cipherPskEnable = 1
+cipherDisable = 0
+
+header = ["S", "Y", "L", "M"] # 1 byte por cada caracter (tiene que coincidir con la cabecera con la que se ocultó el contenido)
+
+
+# DATOS DEL USUARIO:
+pathFileWav = "estego.wav"
+pathFileResultado = "resultado"
+
+
+sizeFileToHide = 0 
+sizeFileWav = 0
+
+
+# Abrimos el archivo WAV que oculta algo
+fileAudioBase = wave.open(pathFileWav,'rb')
 bytesFileAudioBase = bytearray(fileAudioBase.readframes(fileAudioBase.getnframes()))
-print len(bytesFileAudioBase) / 4 #2 canales de 16 bits cada unos (por eso se divide entre 4 para compararlo con en num de frames)
-print fileAudioBase.getnframes()
 
-print fileAudioBase.getsampwidth() # bytes del tamaño del sample
+channels = fileAudioBase.getnchannels() # canales de audio
+sampleWidth = fileAudioBase.getsampwidth() # número de bytes para las muestras del samplerate
+frameRate = fileAudioBase.getframerate()
+numTotalFrames = fileAudioBase.getnframes()
 
-inicioLectura = 0
-saltos=1
-if fileAudioBase.getsampwidth() == 2:
-	inicioLectura = 0
-	saltos = 2
+
+
+startIn = 0
+indexHeader = 0
+indexSampleWav = startIn
+saltos = (sampleWidth * jumpsData)
+bytesFileOculto = []
+indexFileOculto = 0
+
+error = 0
+
+# Leemos cabecera:
+for i in range(len(header)):
+	
+	#bytesFileOculto.append(bytesFileAudioBase[indexSampleWav])
+	#indexFileOculto += 1
+	print header[i], chr(bytesFileAudioBase[indexSampleWav])
+	if header[i] != chr(bytesFileAudioBase[indexSampleWav]):
+		error = 1
+		print "Cabecera no válida"
+		exit(error)
+	indexSampleWav += saltos
+
+
+print "Cabecera válida (OK!)"
+
+# Leemos el tipo de cifrado que utilizará el fichero resultante:
+typeCipher = bytesFileAudioBase[indexSampleWav]
+indexSampleWav += saltos
+
+
+listTamanio = []
+# Leemos los bytes que determinan el tamaño en bytes del fichero:
+for i in range(numFraSize):
+	listTamanio.append(bytesFileAudioBase[indexSampleWav])
+	indexSampleWav += saltos
+
+print "Bytes tamaño fichero oculto: ", listTamanio
+listTamanio.reverse()
+print "Bytes tamaño reordenados: ", listTamanio
+
+desplazamiento = 0
+tamanio = bin(0 & 0xFF)[2:]
+for i in range(numFraSize):
+	print tamanio
+	tamanio += bin(listTamanio[i] & 0xFF)[2:]
+	desplazamiento += 8
+
+sizeFileHidden = int(tamanio, 2)
+print sizeFileHidden
 
 bytesFileOculto = []
+indexDataHidden = 0
+while indexDataHidden < sizeFileHidden:
+	bytesFileOculto.append(bytesFileAudioBase[indexSampleWav])
+	indexSampleWav += saltos
+	indexDataHidden += 1
 	
-indexFileOculto = 0
-indexAudioBase = inicioLectura
-while indexFileOculto < 55952:
-	bytesFileOculto.append(bytesFileAudioBase[indexAudioBase])
-	indexAudioBase += saltos
-	indexFileOculto += 1
 	
-print len(bytesFileOculto)
 ficheroRecuperado = open("recuperado", 'wb')
 newFileByteArray = bytearray(bytesFileOculto)
 ficheroRecuperado.write(newFileByteArray)
